21335730ff715149d5a09e1d4bef2f39d42ea5c3
gulliano <ggulliano@msn.com>
2023-09-25 12:11:04 -0400

    players

--- src/redux/player.js
+++ src/redux/player.js
@@ -1,8 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit"
 
 
 const initState = {
-                players  : [{name:'Ludmilla'} , {name:'Hervé'} , { name : 'Anne'}],
+                players  : [{id:1   , name:'Ludmilla'} , 
+                            { id:2  , name:'Hervé'} , 
+                            { id:3  , name : 'Anne'}],
                 position : 0 ,
 }
 
-ex
+export const player = createSlice({
+    name:'player',
+    initialState:initState,
+    reducers:{
+        addPlayer:(state,action)=>{ //  ajouter un joueur
+            return state ;
+        },
+       delPlayer:(state,action)=>{ //  s un joueur
+            return state ;
+        },
+       resetPlayer:(state,action)=>{ //  vide la liste
+            return state ;
+        },
+    }
+
+})
+
+export const {  addPlayer, delPlayer , resetPlayer } = player.actions ;
+
+export default player.reducer;
